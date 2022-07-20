fa07087abd8200542d2310843b74f4001f0ee488
Omnivers <Twersimor.stock@outlook.com>
2022-07-20 04:09:27 +0200

    GameOfThrones card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React from 'react';
+import Character from './components/Characters';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      characters: [],
+      favorites :[],
+      All:true,
+      favorite:false
+    }
+  }
+  async componentDidMount() {
+      const response = await fetch("https://thronesapi.com/api/v2/Characters")
+      const data = await response.json()
+      this.setState({
+        characters:data
+      })
+    }
+
+  favoriteAffiche =()=>{
+    this.setState({
+      All:false,
+      favorite:true
+    })
+  }
+  AllAffiche =()=>{
+    this.setState({
+      All:true,
+      favorite:false
+    })
+  }
+	render() {
+		return(
+      <div className='content'>
+        <h1 className='title m-2'>Game<span>of</span>thrones</h1>
+        <section className='m-3'>
+            <button className='filter btn m-3' onClick={this.favoriteAffiche}> Favorites</button>
+            <button className='filter btn m-3' onClick={this.AllAffiche}>ALL</button>
+        </section>
+      {this.state.All===true ? (
+        <div className='container row justify-content-center'>{this.state.characters.map((character)=>(
+            <Character key={character.id} name={character.fullName} title={character.title} image={character.imageUrl} handleFavoriteClick={()=>{this.state.favorites.push(character)}} favorites={this.state.favorites}/>
+            ))}
+        </div> ) : (
+           <div className='container row justify-content-center'>{this.state.favorites.map((favoriteCharacter)=>(
+            <Character key={favoriteCharacter.id} name={favoriteCharacter.fullName} title={favoriteCharacter.title} image={favoriteCharacter.imageUrl} />
+            ))}
+            </div>
+            )
+      
+      }
+      </div> 
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ src/components/Characters.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+class Character extends React.Component {
+	render() {
+		return(
+         <div className='card m-4 col-6 '>
+             <img src={this.props.image} className="card-img-top" alt={this.props.name}/>
+                <div className='card-body d-flex flex-column'>
+                    <h1 className='card-title'>{this.props.name}</h1>
+                    <p className='card-text'>{this.props.title}</p>   
+                </div>
+                <button className="btn filter btn-primary m-3" onClick={this.props.handleFavoriteClick}>
+                <i className="fa-solid fa-heart"></i></button>
+        </div>
+		)
+	}
+}
+
+export default Character
